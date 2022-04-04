36bc113e13a8f63d015fd057b2a4a5477e3da628
938938 <938kjh@gmail.com>
2022-04-04 20:58:58 +0900

    remove

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+import { useRef, useState } from 'react';
 
-const dumyList = [
-  {
-    id:1,
-    author:"abc",
-    content:"hihihi",
-    emotion:1,
-    created_date:new Date().getTime(),
-  },
-  {
-    id:2,
-    author:"abc",
-    content:"hihihi",
-    emotion:1,
-    created_date:new Date().getTime(),
-  },
-  {
-    id:3,
-    author:"abc",
-    content:"hihihi",
-    emotion:1,
-    created_date:new Date().getTime(),
-  },
-  {
-    id:4,
-    author:"abc",
-    content:"hihihi",
-    emotion:1,
-    created_date:new Date().getTime(),
-  },
-]
+// const dumyList = [
+//   {
+//     id:1,
+//     author:"abc",
+//     content:"hihihi",
+//     emotion:1,
+//     created_date:new Date().getTime(),
+//   },
+//   {
+//     id:2,
+//     author:"abc",
+//     content:"hihihi",
+//     emotion:1,
+//     created_date:new Date().getTime(),
+//   },
+//   {
+//     id:3,
+//     author:"abc",
+//     content:"hihihi",
+//     emotion:1,
+//     created_date:new Date().getTime(),
+//   },
+//   {
+//     id:4,
+//     author:"abc",
+//     content:"hihihi",
+//     emotion:1,
+//     created_date:new Date().getTime(),
+//   },
+// ]
 
 function App() {
+  const [data,setData] = useState([]);
+  const dataId = useRef(0);
+  const onCreate = (author,content,emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem,...data]);
+  }
+  const onRemove = (targetId) =>{
+    console.log(`${targetId}가 삭제되었습니다`);
+    const newDiaryList = data.filter((it)=>it.id !== targetId);
+    setData(newDiaryList);
+  }
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dumyList}/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} onRemove={onRemove}/>
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,8 +1,14 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) =>{
+const DiaryItem = ({author, content, created_date, emotion, id, onDelete}) =>{
   return(
     <div className="DiaryItem">
         <div className="info">
           <span>작성자 : {author} | 감정점수 : {emotion}</span>
+          <button onClick={()=>{
+            console.log(id);
+            if(window.confirm(`${id}번 일기를 정말 삭제하겠습니까?`))(
+              onDelete(id)
+            );
+          }}>X</button>
           <br />
           <span className="date">
             {new Date(created_date).toLocaleString()}
